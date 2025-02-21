3c82edda116b95bd4a22f2ce35b32f99936d26f0
Arjay Bayona <arjayby1@gmail.com>
2025-02-22 05:08:48 +0800

    add home, login, sign-up pages

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+import { SignUpForm } from "@/components/sign-up-form";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <h1 className="text-center font-instrument-serif text-[64px] font-normal italic leading-[83px]">
+          <Link href="/">Litelaro</Link>
+        </h1>
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
