a756af21554a0871c502da00ed047143186e0a62
Arjay Bayona <arjayby1@gmail.com>
2025-03-04 21:49:22 +0800

    update to new project id

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,10 +1,10 @@
 export const DEFAULT_AVATARS = [
-  "https://lkgyvkfyxiwfsytbztdj.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-1.png",
-  "https://lkgyvkfyxiwfsytbztdj.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-2.png",
-  "https://lkgyvkfyxiwfsytbztdj.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-3.png",
-  "https://lkgyvkfyxiwfsytbztdj.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-4.png",
-  "https://lkgyvkfyxiwfsytbztdj.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-1.png",
-  "https://lkgyvkfyxiwfsytbztdj.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-2.png",
-  "https://lkgyvkfyxiwfsytbztdj.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-3.png",
-  "https://lkgyvkfyxiwfsytbztdj.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-4.png",
+  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-1.png",
+  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-2.png",
+  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-3.png",
+  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-4.png",
+  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-1.png",
+  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-2.png",
+  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-3.png",
+  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-4.png",
 ];
