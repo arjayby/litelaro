ee61763da3d4fdf7c5864095436760ea9ec9f3c0
Arjay Bayona <arjayby1@gmail.com>
2025-03-26 04:33:08 +0800

    refactor(avatars): update default avatar URLs to use Cloudinary
    The default avatar URLs have been updated to use Cloudinary instead of Supabase. This change improves the reliability and performance of avatar loading by leveraging Cloudinary's CDN capabilities.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,10 +1,10 @@
 export const DEFAULT_AVATARS = [
-  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-1.png",
-  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-2.png",
-  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-3.png",
-  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-male-4.png",
-  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-1.png",
-  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-2.png",
-  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-3.png",
-  "https://xbtoocqasvreocszchyb.supabase.co/storage/v1/object/public/avatars/notion-avatar-female-4.png",
+  "https://res.cloudinary.com/dnvqsw8l8/image/upload/v1742934241/notion-avatar-male-1_kzzbc5.png",
+  "https://res.cloudinary.com/dnvqsw8l8/image/upload/v1742934241/notion-avatar-male-2_qxeca5.png",
+  "https://res.cloudinary.com/dnvqsw8l8/image/upload/v1742934242/notion-avatar-male-3_ilsmz7.png",
+  "https://res.cloudinary.com/dnvqsw8l8/image/upload/v1742934242/notion-avatar-male-4_rzodry.png",
+  "https://res.cloudinary.com/dnvqsw8l8/image/upload/v1742934241/notion-avatar-female-1_ba3mm3.png",
+  "https://res.cloudinary.com/dnvqsw8l8/image/upload/v1742934241/notion-avatar-female-2_ummo2n.png",
+  "https://res.cloudinary.com/dnvqsw8l8/image/upload/v1742934241/notion-avatar-female-3_y5ww5u.png",
+  "https://res.cloudinary.com/dnvqsw8l8/image/upload/v1742934241/notion-avatar-female-4_azilui.png",
 ];
